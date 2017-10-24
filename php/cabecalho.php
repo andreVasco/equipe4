1221d911abbe385a441b16cd58c666227a11ebcd
Fernanda <fernandananda95@yahoo.com.br>
2017-10-24 04:25:38 -0200

    Adicionando nova pasta de php

---
+++ php/cabecalho.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    	<meta charset="utf-8">
+	    <meta name="viewport"  content="width=device-width,initial-scale=1"> <!--Ajusta o site para o tamanho do dispositivo-->
+		<title>OREMAN MOTORS</title>
+		<link rel="stylesheet" type="text/css" href="../essets/bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="../essets/css/login.css">
+  </head>
+  <body>
+  	  
