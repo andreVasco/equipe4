c17fb02c296849fa282a32dde6debe46ec45632a
davirezende1998 <davijf_404@hotmail.com>
2017-10-30 20:54:24 -0200

    atualizando  mail to admin

--- php/enviacontato.php
+++ php/enviacontato.php
@@ -1,38 +1,60 @@
 <?php
 session_start();
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$mensagem = $POST['mensagem'];
 
-require 'PHPMailer.php';
-require 'SMTP.php';
-require 'Exception.php';
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$mensagem = $_POST["mensagem"];
 
+use PHPMailer\PHPMailer\PHPMailer;
+require '../PHPMailer-master/src/SMTP.php';
+
+//Create a new PHPMailer instance
 $mail = new PHPMailer();
-$mail -> isSMTP();
-$mail->Host = 'oremanmotors@gmail.com';
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
 $mail->Port = 587;
-$mail->SMTPSecure='tls';
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
 $mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
 $mail->Username = "oremanmotors@gmail.com";
-$mail->Password ='codejrequipe4';
 
-$mail->setFrom("oremanmotors@gmail.com","Admin code");
-$mail->addAddress("oremanmotors@gmail.com");
-$mail->Subject="Email de contato da loja";
-$mail->msgHTML("<html>de: {$nome}<br/>email: {$email}<br/>mensagem:{$mensagem}</html>");
-$mail->AltBody ="de {$nome}\nemail:{$email}\nmensagem:{$mensagem}";
+//Password to use for SMTP authentication
+$mail->Password = "codejrequipe4";
 
-if($mail->send()){
+//Set who the message is to be sent from
+$mail->setFrom($email);
+$mail->addAddress('oremanmotors@gmail.com', 'EQUIPE OREMAN');
 
-	$_SESSION["success"]="Mensagem enviada com sucesso";
-	header("Location:index.php");
-}else{
+//Set the subject line
+$mail->Subject = "Mensagem do cliente";
 
-	$_SESSION["danger"]="Erro ao enviar mensagem".$mail->ErrorInfo;
-	header("Location:contato.php");
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML("<html>de: {$nome} <br/>email: {$email} <br/>mensagem: {$mensagem}</html>");
+$mail->AltBody = " de: {$nome} \n email: {$email} \n mensagem: {$mensagem}";
 
+//Send email and verify success/error
+if ($mail->send()) {
+   $_SESSION["success"] = "Sua mensagem foi enviada!";
+   header("Location: ../html/home.html");
+} else {
+   $_SESSION["danger"] = "ERRO: Sua mensagem não foi enviada!" . $mail->ErrorInfo;
+    header("Location:contato.php");
 }
 
-
-?>
+die();
