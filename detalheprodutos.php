ac2898d295fe7d21e3f194a059789d27bf753903
thrallorien <31359644+thrallorien@users.noreply.github.com>
2017-11-02 17:20:05 -0200

    nova
    nova

--- detalheprodutos.php
+++ detalheprodutos.php
@@ -13,7 +13,7 @@ $resultado = mysqli_query($conexao, "select * from produtos");
 		<link rel="icon" href="../assets/images/icon.png" type="image/x-icon"/><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="../assets/css/animate.css">
 		<link rel="icon" href="../assets/images/icon.png" type="image/x-icon"/>
-        <link rel="stylesheet" type="text/css" href="../assets/css/footer.css">
+       		<link rel="stylesheet" type="text/css" href="../assets/css/footer.css">
 		<link rel="stylesheet" type="text/css" href="../assets/css/menu1.css">
 	<title>DETALHES DO PRODUTO</title>
 
