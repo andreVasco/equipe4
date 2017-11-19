28015596f2c8f5eafba5e3a40f723a57318f3e1d
Fernanda <fernandananda95@yahoo.com.br>
2017-11-19 00:21:56 -0200

    localização

--- php/localizacao.php
+++ php/localizacao.php
@@ -47,23 +47,27 @@
 		</header>
 		<!--FIM DO MENU-->
 		<!--MAPA E ENDEREÇO-->
-		<section  id="teste-js" class="container-fluid " >
-			<div  class=" complemento  animated bounceInLeft">
+		
+		<section class="teste">
+		<section  id="teste-js" class="container-fluid  " >
 				<!-- <h2 id="titulo-h2-localizacao" >Localização:</h2> -->
 					<address>
 					<div id="endereco-localizacao">
-						 <div id="oreman"> <strong>Oreman Motors</strong><br></div>
+						  <strong>Oreman Motors</strong><br>
 						  Via local, nº xx, São Pedro<br>
 						  Juiz de fora-MG<br>
-						  <abbr title="Phone">Telefone:</abbr> (XX) XXXX-XXXX<br>
-						</address>
+						  Telefone: (XX) XXXX-XXXX<br>
+					</address>
 					</div>
-			</div>
-					<iframe id="mapa-localizacao" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3705.1161857728302!2d-43.37458818505456!3d-21.775763885598504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x989ba18e76c0b9%3A0xb27885fe8e1192df!2sOreman+Motors!5e0!3m2!1spt-BR!2sus!4v1507832094067" width="600" height="450" frameborder="0" style="border:0" allowfullscreen class="img-responsive center-block ">
+					<div id="mapa-loc">
+					<iframe id="mapa-localizacao" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3705.1161857728302!2d-43.37458818505456!3d-21.775763885598504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x989ba18e76c0b9%3A0xb27885fe8e1192df!2sOreman+Motors!5e0!3m2!1spt-BR!2sus!4v1507832094067" width="600" height="450" frameborder="0" style="border:0" allowfullscreen class="img-responsive ">
 					</iframe>
+					</div>
 		</section>
+
 		<!--MAPA E ENDEEÇO-->
 	</body>
+	</section>
 		<!--FOOTER-->
 	<div id="body-footer" class="footer-localizacao-loc">
 	<!-- Footer comeca aqui -->
@@ -86,6 +90,7 @@
 			</div>
 		</div>
 	</footer>
+	</div>
 	<!-- Footer termina aqui -->
 	<script src="../assets/js/jquery.js"></script>
 	<!--adicionando o popper que estava apareceendo faltando na página-->
